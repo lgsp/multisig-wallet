98e06296fa04a5ff2c0e888d58f2fca0c7e013b1
Laurent Garnier <laurent.garnier.superprof@gmail.com>
2022-04-24 19:19:09 +0200

    Lecture 4 Your First Solidity Test DONE!

--- test/wallet.js
+++ test/wallet.js
@@ -6,4 +6,15 @@ contract('Wallet', (accounts) => {
 	wallet = await Wallet.new([accounts[0], accounts[1], accounts[2]], 2);
 	await web3.eth.sendTransaction({from: accounts[0], to: wallet.address, value: 1000});
     });
+
+    it('should have correct approvers and quorum', async () => {
+	const approvers = await wallet.getApprovers();
+	const quorum = await wallet.quorum();
+	assert(approvers.length === 3);
+	assert(approvers[0] === accounts[0]);
+	assert(approvers[1] === accounts[1]);
+	assert(approvers[2] === accounts[2]);
+	assert(quorum.toNumber() === 2);
+	
+    })
 });
